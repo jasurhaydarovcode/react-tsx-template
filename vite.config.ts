e8aff6749fda8bee0438194df92295d5612cc309
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-04 08:39:34 +0500

    Vite Config Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin, ViteDevServer } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// console plugin
+const Project_NAME = (): Plugin => {
+  return {
+    name: 'console-plugin',
+    configureServer(server: ViteDevServer) {
+      server.httpServer?.once('listening', () => {
+        console.log('\n Happy Hacking, 🫠 🫠 😴\n Project_NAME\n');
+      });
+    },
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Project_NAME()],
+  define: {
+    global: 'window',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 99
+  }
 })
